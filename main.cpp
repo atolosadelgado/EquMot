cf7508a07395d48076f0a5a6b8c622082550c922
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2022-12-11 16:26:25 +0100

    Euler integration of motion equations, 2 particles

--- main.cpp
+++ main.cpp
@@ -4,26 +4,45 @@
 #include <cmath>
 #include <random>
 #include <chrono>
+#include <thread>
 
 unsigned seed = 1; // std::chrono::system_clock::now().time_since_epoch().count();
 std::default_random_engine generator (seed);
 std::uniform_real_distribution<double> rnd_position (-100.,100.);
 
+
+
+
 class TVector
 {
 public:
-    TVector(){};
-    TVector(double ix, double iy):x(ix),y(iy){};
+    TVector() {};
+    TVector(double ix, double iy):x(ix),y(iy) {};
     double x = {0.0};
     double y = {0.0};
 
-    void Set(  TVector & v ){ x = v.x; y=v.y; return; };
-    void SetRnd(){x=rnd_position(generator); y=rnd_position(generator);};
+    void Set(  TVector & v ) {
+        x = v.x;
+        y=v.y;
+        return;
+    };
+    void SetRnd() {
+        x=rnd_position(generator);
+        y=rnd_position(generator);
+    };
 
-    double Norm() const { return std::sqrt( pow(x,2) + pow(y,2)); }
-    double Norm2() const { return  pow(x,2) + pow(y,2); }
-    double distance( TVector & v){ return std::sqrt( pow(x -v.x,2) + pow(y-v.y,2)); }
-    static double distance( TVector & v,  TVector & vv){ return std::sqrt( pow(vv.x -v.x,2) + pow(vv.y-v.y,2)); }
+    double Norm() const {
+        return std::sqrt( pow(x,2) + pow(y,2));
+    }
+    double Norm2() const {
+        return  pow(x,2) + pow(y,2);
+    }
+    double distance( TVector & v) {
+        return std::sqrt( pow(x -v.x,2) + pow(y-v.y,2));
+    }
+    static double distance( TVector & v,  TVector & vv) {
+        return std::sqrt( pow(vv.x -v.x,2) + pow(vv.y-v.y,2));
+    }
 
     TVector & operator * (double const & scale) {
         this->x*=scale;
@@ -32,11 +51,11 @@ public:
     }
 
     friend std::ostream& operator<<(std::ostream& os, const TVector& v);
-    TVector& operator+=(const TVector& v){
+    TVector& operator+=(const TVector& v) {
 
-      this->x += v.x;
-      this->y += v.y;
-      return *this;
+        this->x += v.x;
+        this->y += v.y;
+        return *this;
     }
 
 };
@@ -50,19 +69,87 @@ std::ostream& operator<<(std::ostream& os, const TVector& v)
 class TParticle
 {
 public:
-    TParticle (){ pos.SetRnd(); vel.x = 0.1* pos.y; vel.y = -0.1*pos.x; id = (++nparticles); };
+    TParticle () {
+        pos.SetRnd();
+        vel.x = 0.01* pos.y;
+        vel.y = -0.01*pos.x;
+        id = (++nparticles);
+    };
     TVector pos;
     TVector vel;
     int id = {0};
     static int nparticles;
+    static double f_constant;
 
-    double Kene() const {return 0.5*vel.Norm2(); }
-    TVector Force( TVector & ipos){ auto d = pos.distance(ipos); d=pow(d,3); return TVector( -(ipos.x - pos.x)/d, -(ipos.y - pos.y)/d); };
+    double Kene() const {
+        return 0.5*vel.Norm2();
+    }
+    TVector Force( TVector & ipos) {
+        auto d = pos.distance(ipos);
+        d=pow(d,3);
+        return TVector( -f_constant*(ipos.x - pos.x)/d, -f_constant*(ipos.y - pos.y)/d);
+    };
 
     friend std::ostream& operator<<(std::ostream& os, const TParticle& v);
 };
 
 int TParticle::nparticles = 0;
+double TParticle::f_constant = 100.;
+
+class myGNUplotter
+{
+public:
+    myGNUplotter();
+    ~myGNUplotter();
+    void StartPlot();
+    void AddPoint( TVector & v);
+    void ShowPlot();
+    FILE * plotHandle;
+};
+
+myGNUplotter::myGNUplotter()
+{
+    plotHandle = popen("gnuplot -persist", "w");
+
+    if(plotHandle == NULL)
+    {
+        std::runtime_error("Can not open pipe to GNU plot");
+    }
+
+    fprintf(plotHandle,"set terminal qt\n");
+    fprintf(plotHandle,"set xlabel \"x\"\n");
+    fprintf(plotHandle,"set ylabel \"y\"\n");
+    fprintf(plotHandle,"set xrange [-100:100]\n");
+    fprintf(plotHandle,"set yrange [-100:100]\n");
+    fprintf(plotHandle,"set grid\n");
+}
+
+void myGNUplotter::StartPlot()
+{
+    fprintf( plotHandle, "plot '-'\n");
+}
+
+
+void myGNUplotter::AddPoint(TVector& v)
+{
+    fprintf( plotHandle, "%g %g\n", v.x, v.y);
+}
+
+void myGNUplotter::ShowPlot()
+{
+    fprintf( plotHandle, "e\n");
+    fflush(plotHandle);
+}
+
+myGNUplotter::~myGNUplotter()
+{
+    fprintf( plotHandle, "exit\n");
+    pclose(plotHandle);
+}
+
+
+
+
 
 std::ostream& operator<<(std::ostream& os, const TParticle& v)
 {
@@ -71,18 +158,30 @@ std::ostream& operator<<(std::ostream& os, const TParticle& v)
     return os;
 }
 
-int main(){
+int main() {
     std::cout << "Hello world!\n";
 
     TParticle a;
     TParticle b;
 
     double h = 1;
+    myGNUplotter plt;
 
     for( int i = 0; i < 100; ++i)
     {
         std::cout << a << std::endl;
         std::cout << b << std::endl;
+
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        plt.StartPlot();
+
+        plt.AddPoint( a.pos );
+        plt.AddPoint( b.pos );
+
+
+        plt.ShowPlot();
+
 
         TVector forcea = a.Force(b.pos);
         TVector forceb = b.Force(a.pos);
@@ -101,32 +200,14 @@ int main(){
 
 
 
+
     std::cout << a << std::endl;
     std::cout << b << std::endl;
     std::cout << a.Force(b.pos) << std::endl;
 
     std::cout << (a.pos+=b.pos) << std::endl;
 
-  FILE *plotHandle = popen("gnuplot -persist", "w");
 
-  if(plotHandle == NULL){
-   return -1;
-  }
 
-  fprintf(plotHandle,"set terminal qt\n");
-  fprintf(plotHandle,"set xlabel \"frequency\"\n");
-  fprintf(plotHandle,"set ylabel \"Amplitude\"\n");
-  fprintf(plotHandle,"set xrange [-3:3]\n");
-  fprintf(plotHandle,"set grid\n");
-  fprintf(plotHandle,"plot (x**2)*sin(x*100)\n");
-// plot '-' w p ls 1, '-' w p ls 2, '-' w p ls 3
-// 1 2
-// e
-// 2 1
-// e
-
-  fflush(plotHandle);
-
-  pclose(plotHandle);
 
 }

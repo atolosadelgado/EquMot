4a78dd01fe37ce75625b6bc039c0a11ff5cd53d0
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2022-12-12 18:57:58 +0100

    TIntegrator moved to include+src

---
+++ include/TIntegrator.hpp
@@ -0,0 +1,38 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// //  Alvaro Tolosa Delgado 2022
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+
+#ifndef __TINTEGRATOR_HPP__
+#define __TINTEGRATOR_HPP__
+
+#include "TParticle.hpp"
+
+#include "TPlot.hpp"
+
+class TIntegrator
+{
+public:
+
+    TIntegrator();
+
+    std::vector<TParticle> particle_v;
+
+    void DoStep();
+    void PlotPositions(TPlot & plt);
+    double h = {0.1};
+    int step = {0};
+    int nrefresh = {5};
+
+    void SetNparticlesRnd(int n);
+
+    double GetMene(double * Kene = nullptr, double * Vene = nullptr);
+    void PrintMene();
+    void CheckDistances();
+
+    void IntegratorEulerFw( std::vector<TVector> & forces);
+
+    void IntegratorVerlet(std::vector< TVector >& force_v);
+};
+
+#endif

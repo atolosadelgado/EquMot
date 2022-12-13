d8d361f2726bbe7960888eb1e186310828dbbfe7
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2022-12-13 23:39:03 +0100

    Solar System simulation. In case of Earth-Sun, fluctuations of Mene lower than 0.1% rel In case of Earth-Sun, Jupiter, Saturn, fluctuations of Mene of 0.25% rel

--- main.cpp
+++ main.cpp
@@ -91,24 +91,33 @@ void Test_ensemble2()
 
 }
 
-const double mass_earth = 5.972e24; //kg
 const double mass_sun   = 1.989e30; //kg
-
 const double UA_m       =1.495978707e11; //m
 
 const double G_SI       = 6.6743e-11; // m3 kg-1 s-2
-
 const double G_UA_MSun  = G_SI / pow(1.495978707, 3) * 1.989e-3; // UA3 msun-1 s-2
 
+const double mass_earth = 5.972e24; //kg
 const double earth_obital_speed = 29.78e3; // m/s
+const double earth_obital_speed_UA = 29.78e3/UA_m; // UA/s
 
-const double earth_obital_speed_UA = 29.78e3/UA_m; // m/s
+
+
+const double saturn_distance = 9.5; //UA
+const double saturn_mass= 5.683e26; //kg
+const double saturn_orbital_speed =9.69e3; // m/s
+const double saturn_orbital_speed_UA = saturn_orbital_speed/UA_m; // UA/s
+
+
+const double distance_jupiter = 5.2; //UA
+const double mass_jupiter = 1.898e27; //kg
+const double jupiter_orbital_speed = 13.07e3; // m/s
+const double jupiter_orbital_speed_UA = jupiter_orbital_speed/UA_m; // UA/s
+
 
 const double seconds_per_year = 365.26*24*3600;
-
 const double G_UA_MSun_yr = G_SI / pow(1.495978707, 3) * 1.989e-3 * pow(seconds_per_year,2); // UA3 msun-1 s-2
-
-const double earth_obital_speed_UA_yr = 29.78e3/UA_m*seconds_per_year; // m/s
+const double earth_obital_speed_UA_yr = earth_obital_speed/UA_m*seconds_per_year; // m/s
 
 
 int main()
@@ -118,11 +127,11 @@ int main()
 
     TIntegrator myIntegrator;
     myIntegrator.h = 3600;
-    myIntegrator.nrefresh = 10;
+    myIntegrator.nrefresh = 100;
 
     TParticle::f_constant = G_UA_MSun;
 
-    myIntegrator.SetNparticlesRnd(2);
+    myIntegrator.SetNparticlesRnd(4);
 
     // Sun
     myIntegrator.particle_v[0].pos.x = 0;
@@ -142,11 +151,27 @@ int main()
     myIntegrator.particle_v[1].pos.y = 0;
     myIntegrator.particle_v[1].vel.x = 0;
     myIntegrator.particle_v[1].vel.y = earth_obital_speed_UA;
-
     myIntegrator.particle_v[1].mass =  mass_earth / mass_sun;
-
-
     std::cout << myIntegrator.particle_v[1] << std::endl;
+
+
+
+    // Jupiter
+    myIntegrator.particle_v[2].pos.x = -5.2;
+    myIntegrator.particle_v[2].pos.y = 0;
+    myIntegrator.particle_v[2].vel.x = 0;
+    myIntegrator.particle_v[2].vel.y = -jupiter_orbital_speed_UA;
+    myIntegrator.particle_v[2].mass =  mass_jupiter / mass_sun;
+    std::cout << myIntegrator.particle_v[2] << std::endl;
+
+
+    // Saturn
+    myIntegrator.particle_v[3].pos.x = -saturn_distance;
+    myIntegrator.particle_v[3].pos.y = 0;
+    myIntegrator.particle_v[3].vel.x = 0;
+    myIntegrator.particle_v[3].vel.y = -saturn_orbital_speed_UA;
+    myIntegrator.particle_v[3].mass =  saturn_mass / mass_sun;
+    std::cout << myIntegrator.particle_v[3] << std::endl;
 
     myIntegrator.PlotPositions(plt);
 
@@ -154,15 +179,15 @@ int main()
 
     std::ofstream ofile("Verlet_2.txt");
 
-    for( int i = 0; i < 50000; ++i)
+    for( int i = 0; i < 500000; ++i)
     {
         myIntegrator.DoStep();
         myIntegrator.CheckDistances();
         myIntegrator.PlotPositions(plt);
-        myIntegrator.PrintMene();
+//         myIntegrator.PrintMene();
 
 //         std::cin.ignore();
-        ofile << myIntegrator.GetMene() << std::endl;
+        if( 0 == i % 10 )  ofile << myIntegrator.GetMene() << std::endl;
 
     }
 

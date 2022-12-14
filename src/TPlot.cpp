5d753e2cea76af2da427adc4ac26d85afa712261
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2022-12-14 19:42:41 +0100

    Safe guard variable GNUplot is defined in CMakeLists.txt, so gnuplot code will not be executed if there is no gnuplot available

--- src/TPlot.cpp
+++ src/TPlot.cpp
@@ -7,6 +7,8 @@
 
 TPlot::TPlot()
 {
+
+#ifdef GNUPLOT
     plotHandle = popen("gnuplot -persist", "w");
 
     if(plotHandle == NULL)
@@ -20,29 +22,40 @@ TPlot::TPlot()
     fprintf(plotHandle,"set xrange [-1.2:1.2]\n");
     fprintf(plotHandle,"set yrange [-1.2:1.2]\n");
     fprintf(plotHandle,"set grid\n");
+#endif
+
 }
 
 void TPlot::StartPlot()
 {
+#ifdef GNUPLOT
 //     fprintf( plotHandle, "plot '-'\n");
     fprintf( plotHandle, "plot '-' w circles lc 1 fs solid\n");
+#endif
+
 }
 
 
 void TPlot::AddPoint(TVector& v)
 {
+#ifdef GNUPLOT
     fprintf( plotHandle, "%g %g 0.05\n", v.x, v.y);
+#endif
 }
 
 void TPlot::ShowPlot()
 {
+#ifdef GNUPLOT
     fprintf( plotHandle, "e\n");
     fflush(plotHandle);
+#endif
 }
 
 TPlot::~TPlot()
 {
+#ifdef GNUPLOT
     fprintf( plotHandle, "exit\n");
     pclose(plotHandle);
+#endif
 }
 

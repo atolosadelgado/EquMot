a9c7be9265f4717efb58e060312dade08920ff5d
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2022-12-29 12:47:50 +0100

    electrostatic force now included in TParticle

--- include/TParticle.hpp
+++ include/TParticle.hpp
@@ -8,6 +8,9 @@
 
 #include "TVector.hpp"
 
+#include <functional>
+#include <numeric>
+
 /// Class to store properties of a particle
 class TParticle
 {
@@ -22,6 +25,9 @@ public:
 
     /// Mass
     double mass = {1.0};
+
+    /// Charge
+    double charge = {0.0};
 
     /// force
     TVector force;
@@ -38,11 +44,37 @@ public:
     /// Constant of the force
     static double f_constant;
 
+    /// Constant of the electric force
+    static double fE_constant;
+
+    /// Constant of the gravitational force
+    static double fG_constant;
+
+    /// Constant of the elastic force
+    static double fL_constant;
+
+    /// Compute force only from particles inside the radius frad_critical
+    static double frad_critical;
+
     /// Compute Kinetic energy
     double Kene() const ;
 
     /// Compute force that would be applied to another particle
-    inline TVector Force(TParticle& ipar);
+    std::function< TVector(TParticle& ipar)> Force;
+
+    void SetForce(int n);
+
+    /// Compute the Electric force that would be applied to another particle
+    inline TVector ForceE(TParticle& ipar);
+
+    /// Compute the Electric force that would be applied to another particle
+    inline TVector ForceG(TParticle& ipar);
+
+    /// Compute the elasticforce that would be applied to another particle
+    inline TVector ForceL(TParticle& ipar);
+
+    /// Compute LJ force
+    inline static TVector ForceLJ( TParticle & a, TParticle & b);
 
     /// Compute the potential that other particle r feels
     double Vene( TParticle & r );
@@ -56,7 +88,23 @@ public:
     friend std::ostream& operator<<(std::ostream& os, const TParticle& v);
 };
 
-inline TVector TParticle::Force( TParticle & ipar ) {
+inline TVector TParticle::ForceE( TParticle & ipar ) {
+//     auto d = pos.distance(ipar.pos);
+//     d=pow(d,3);
+//     return TVector( -f_constant*ipar.mass*mass*(ipar.pos.x - pos.x)/d, -f_constant*ipar.mass*mass*(ipar.pos.y - pos.y)/d);
+//     auto d = pos.distance(ipar.pos);
+//     d=pow(d,3);
+    double dx = ipar.pos.x - pos.x;
+    double dy = ipar.pos.y - pos.y;
+    double d2= dx*dx + dy*dy;
+    double d = sqrt(d2);
+    double fE_mod = 0;
+    if( d < frad_critical )
+        fE_mod = -fE_constant*ipar.charge*charge/d2;
+    return TVector( fE_mod*(dx)/d, fE_mod*(dy)/d);
+};
+
+inline TVector TParticle::ForceG( TParticle & ipar ) {
 //     auto d = pos.distance(ipar.pos);
 //     d=pow(d,3);
 //     return TVector( -f_constant*ipar.mass*mass*(ipar.pos.x - pos.x)/d, -f_constant*ipar.mass*mass*(ipar.pos.y - pos.y)/d);
@@ -68,6 +116,17 @@ inline TVector TParticle::Force( TParticle & ipar ) {
     return TVector( -f_constant*ipar.mass*mass*(dx)/d32, -f_constant*ipar.mass*mass*(dy)/d32);
 };
 
+inline TVector TParticle::ForceL(TParticle& ipar)
+{
+    double dx = ipar.pos.x - pos.x;
+    double dy = ipar.pos.y - pos.y;
+    double d2= dx*dx + dy*dy;
+    double d = sqrt(d2);
+    double fL_mod = 0;
+    if( d < frad_critical )
+        fL_mod = -fL_constant*d;
+    return TVector( fL_mod*(dx)/d, fL_mod*(dy)/d);
+}
 
 
 #endif
--- src/TParticle.cpp
+++ src/TParticle.cpp
@@ -3,13 +3,47 @@
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
 #include "TParticle.hpp"
+#include "Contants.hpp"
 
 TParticle::TParticle () {
     pos.SetRnd();
     vel.x = 0;
     vel.y = 0;
     id = (++nparticles);
+    Force = [&](TParticle & p){return this->ForceG(p); };
 };
+
+TVector TParticle::ForceLJ(TParticle& a, TParticle& ipar)
+{
+    double dx = ipar.pos.x - a.pos.x;
+    double dy = ipar.pos.y - a.pos.y;
+    double r= sqrt( dx*dx + dy*dy);
+    double sigma = 1.0; //nm
+    double epsilon = 1.38e-5; // kg*nm2/s2
+    double sigma_r = sigma/r;
+    double LJ_mod = 0;
+    if( r < frad_critical )
+        LJ_mod = 24*epsilon/sigma*( 2*pow( sigma_r,13) -  pow( sigma_r,7) );
+
+    return TVector( LJ_mod*(dx)/r, LJ_mod*(dy)/r);
+}
+
+
+void TParticle::SetForce(int n)
+{
+    if( 1 == n)
+            Force = [&](TParticle & p){return this->ForceG(p); };
+    if( 2 == n)
+            Force = [&](TParticle & p){return this->ForceE(p); };
+    if( 12 == n)
+            Force = [&](TParticle & p){return (this->ForceE(p)+=this->ForceG(p)) ; };
+    if( 3 == n)
+            Force = std::bind( TParticle::ForceLJ, *this, std::placeholders::_1);
+    if( 4 == n)
+            Force = [&](TParticle & p){return this->ForceL(p); };
+
+}
+
 
 double TParticle::Kene() const {
     return 0.5*mass*vel.Norm2();
@@ -28,7 +62,8 @@ double TParticle::Kene() const {
 // };
 
 double TParticle::Vene( TParticle & ipar ) {
-    return -f_constant*mass*ipar.mass/pos.distance(ipar.pos);
+//     return -f_constant*mass*ipar.mass/pos.distance(ipar.pos);
+    return -fE_constant*charge*ipar.charge/pos.distance(ipar.pos);
 };
 double TParticle::Mene( TParticle & ipar ) {
     return Kene()+Vene(ipar);
@@ -44,7 +79,10 @@ void TParticle::SetFixPosition()
 
 int TParticle::nparticles = 0;
 double TParticle::f_constant = 1000.;
-
+double TParticle::fL_constant = 1.;
+double TParticle::fE_constant = K_SI_e;
+double TParticle::fG_constant = G_SI;
+double TParticle::frad_critical = std::numeric_limits<double>::max();
 
 
 
